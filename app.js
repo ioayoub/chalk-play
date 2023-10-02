cee1a0f169675cae1ebfb54e7bb34c5e31424d86
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2023-10-02 15:57:53 +0200

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+//ESM
+import chalk from "chalk";
+
+console.log(chalk.bgYellow("Hello Wilders"));
+
+export default toto;
+
+// CommonJS
+// require()
+// module.exports = toto
---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.yellow("Hello Wilders"),
+  chalk.red("Hello Wilders"),
+  chalk.blue("Hello Wilders"),
+  chalk.green("Hello Wilders")
+);
